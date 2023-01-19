9be5a21449e8853c6a266b29032ecc4d3ce130ae
hsn2 <912372447@qq.com>
2023-01-19 19:54:43 +0800

    add:salted_hash

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import base64
+import hashlib
 import random
 
 import pyaes
@@ -28,6 +29,13 @@ class AesCrypto:
         for i in range(int(len(data_bytes) / 16)):
             rt_bytes += self.aes.decrypt(data_bytes[16 * i:16 * (i + 1)])
         return rt_bytes.rstrip(b'\x00').decode('utf8')
+
+
+def salted_hash(data, salt, additional_string=None):
+    hash_salt = salt
+    if additional_string is not None:
+        hash_salt += hashlib.sha1(additional_string.encode('utf8')).hexdigest()
+    return hashlib.sha1((data + hash_salt).encode('utf8')).hexdigest()
 
 
 def get_random_token(key_len=128):
