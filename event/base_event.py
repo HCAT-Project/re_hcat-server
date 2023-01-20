fa2c94d8f5850ec0260d1b7a37fba06a408cad77
hsn2 <912372447@qq.com>
2023-01-20 12:06:51 +0800

    update:make create event easier

--- event/base_event.py
+++ event/base_event.py
@@ -1,3 +1,4 @@
+import inspect
 import json
 
 from werkzeug.local import LocalProxy
@@ -9,25 +10,24 @@ from event.event_manager import EventManager
 
 class BaseEvent:
     auth = True
-    requirements = []
 
-    def __init__(self, server, req, path: str, e_mgr:EventManager):
+    def __init__(self, server, req, path: str, e_mgr: EventManager):
         self.req = req
         self.server = server
         self.path = path
-        self.e_mgr=e_mgr
+        self.e_mgr = e_mgr
 
     def run(self):
         req_data = util.request_parse(self.req)
-
-        if util.ins(self.requirements, req_data):
-            if len(self.requirements) > 0:
-                return self._run([req_data[k] for k in self.requirements])
+        requirements = [i for i in inspect.signature(self._run).parameters]
+        if util.ins(requirements, req_data):
+            if len(requirements) > 0:
+                return self._run(*[req_data[k] for k in requirements])
             else:
                 return self._run()
         else:
             return ReturnData(ReturnData.ERROR,
-                              f'Parameters do not meet the requirements:[{",".join(self.requirements)}]').json()
+                              f'Parameters do not meet the requirements:[{",".join(requirements)}]').json()
 
     def _run(self, *args):
         ...
