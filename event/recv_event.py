0d286a7b7ccaab088f1be7dd4c5387c4af299565
hsn2 <912372447@qq.com>
2023-01-20 12:07:54 +0800

    change:print_exc when error

--- event/recv_event.py
+++ event/recv_event.py
@@ -1,3 +1,5 @@
+import traceback
+
 from flask import make_response
 
 from event.base_event import BaseEvent
@@ -6,7 +8,6 @@ import importlib
 
 class RecvEvent(BaseEvent):
     auth = False
-    requirements = []
 
     def _run(self):
         try:
@@ -15,6 +16,14 @@ class RecvEvent(BaseEvent):
                 class_name += i[0].upper() + (i[1:] if len(i) > 0 else '')
 
             event_module = importlib.import_module(f'event.events.{self.path.replace("/", ".")}')
-            return self.e_mgr.create_event(getattr(event_module, class_name), self.req, self.path)
+            event_class = getattr(event_module, class_name)
+
         except:
+            if self.server.debug:
+                traceback.print_exc()
             return make_response('No Found', 404)
+        try:
+            return self.e_mgr.create_event(event_class, self.req, self.path)
+        except Exception:
+            traceback.print_exc()
+            return make_response('Internal Server Error', 500)
