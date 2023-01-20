29af011d1e57337ae26eb960382f7732bd09a5f6
hsn2 <912372447@qq.com>
2023-01-20 14:57:47 +0800

    add:login_event

---
+++ event/events/account/login.py
@@ -0,0 +1,38 @@
+import json
+
+import util
+from containers import ReturnData, User
+from event.base_event import BaseEvent
+from flask import make_response
+
+
+class Login(BaseEvent):
+    auth = False
+
+    def _run(self, user_id, password):
+        if not self.server.is_user_exist(user_id):
+            return ReturnData(ReturnData.NULL, 'User does not exist.').json()
+
+        with self.server.open_user(user_id) as u:
+            user: User = u.value
+            if user.auth(password):
+                # generate token
+                user.token = util.get_random_token()
+
+                # init a response
+                resp = make_response(ReturnData(ReturnData.OK).json(), 200)
+
+                # generate auth_data
+                auth_data = json.dumps({'user_id': user_id, 'token': user.token, 'salt': util.get_random_token()})
+
+                # crypto
+                aes = util.AesCrypto(self.server.key)
+
+                # set a @Yummy_Cookies_S
+                # XD
+                resp.set_cookie('auth_data', aes.encrypto(auth_data))
+
+                # return
+                return resp
+            else:
+                return ReturnData(ReturnData.ERROR, 'Incorrect user ID or password.')
