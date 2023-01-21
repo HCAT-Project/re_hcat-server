d64f2b6535b8181dd715033687ea416485ba609d
hsn2 <912372447@qq.com>
2023-01-21 15:01:43 +0800

    add:get_friend_list

---
+++ event/events/friend/get_friend_list.py
@@ -0,0 +1,13 @@
+import time
+
+from containers import ReturnData, User, EventContainer
+from event.base_event import BaseEvent
+
+
+class GetFriendList(BaseEvent):
+    auth = True
+
+    def _run(self):
+        with self.server.open_user(self.user_id) as u:
+            user: User = u.value
+            return ReturnData(ReturnData.OK).add('data', [i for i in user.friend_dict])
