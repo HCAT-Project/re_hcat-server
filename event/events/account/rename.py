621eaee96373aaa174c45c5474efa44bebbe3936
hsn2 <912372447@qq.com>
2023-01-21 12:33:06 +0800

    add:rename

---
+++ event/events/account/rename.py
@@ -0,0 +1,12 @@
+from containers import User, ReturnData
+from event.base_event import BaseEvent
+
+
+class Rename(BaseEvent):
+    auth = True
+
+    def _run(self, name):
+        with self.server.open_user(self.user_id) as u:
+            user: User = u.value
+            user.user_name = name
+            return ReturnData(ReturnData.OK)
