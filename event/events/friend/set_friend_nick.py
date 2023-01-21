387c681c073b2ca0f8d0591ba7852587c5498a5c
hsn2 <912372447@qq.com>
2023-01-21 15:25:57 +0800

    add:set_friend_nick

---
+++ event/events/friend/set_friend_nick.py
@@ -0,0 +1,14 @@
+from containers import ReturnData, User
+from event.base_event import BaseEvent
+
+
+class SetFriendNick(BaseEvent):
+    auth = True
+
+    def _run(self, friend_id, nick):
+        with self.server.open_user(self.user_id) as u:
+            user: User = u.value
+            if friend_id not in user.friend_dict:
+                return ReturnData(ReturnData.NULL, 'The person is not your friend.')
+            user.friend_dict[friend_id]['nick'] = nick
+            return ReturnData(ReturnData.OK)
