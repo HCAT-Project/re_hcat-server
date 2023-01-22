a33fa7070729af8d458b51051c2828d81fbd0930
hsn2 <912372447@qq.com>
2023-01-22 22:39:25 +0800

    change:auth mode

--- event/event_manager.py
+++ event/event_manager.py
@@ -13,18 +13,21 @@ class EventManager:
     def create_event(self, event, req, path):
         auth_success = True
         j = {'user_id': None}
-        if event.auth:
-            auth_success = False
-            if 'auth_data' in req.cookies:
-                auth_data = req.cookies['auth_data']
-                try:
-                    auth_data_decrypto = AesCrypto(self.server.key).decrypto(auth_data)
-                except:
-                    return ReturnData(ReturnData.ERROR, 'Invalid token.').jsonify()
+
+        if 'auth_data' in req.cookies:
+            auth_data = req.cookies['auth_data']
+            try:
+                auth_data_decrypto = AesCrypto(self.server.key).decrypto(auth_data)
                 j = json.loads(auth_data_decrypto)
                 with self.server.open_user(j['user_id']) as v:
                     user: User = v.value
                     auth_success = user.auth_token(j['token'])
+            except:
+                if event.auth:
+                    return ReturnData(ReturnData.ERROR, 'Invalid token.').jsonify()
+
+        if not event.auth:
+            auth_success = True
         if auth_success:
             e = event(self.server, req, path, self, j['user_id'])
             rt = e.run()
