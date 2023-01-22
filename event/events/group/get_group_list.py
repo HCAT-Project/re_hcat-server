6e3d9bc2fbefc5d24722d473eeb6ef73d3c9d24c
hsn2 <912372447@qq.com>
2023-01-22 22:30:35 +0800

    add:get_group_list

---
+++ event/events/group/get_group_list.py
@@ -0,0 +1,11 @@
+from containers import User, ReturnData
+from event.base_event import BaseEvent
+
+
+class GetGroupList(BaseEvent):
+    auth = True
+
+    def _run(self):
+        with self.server.open_user(self.user_id) as u:
+            user: User = u.value
+            return ReturnData(ReturnData.OK).add('data', list(user.groups_dict))
