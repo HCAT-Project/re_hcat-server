46e14bfb647d9879cd88f907e62db3639c4fe715
hsn2 <912372447@qq.com>
2023-01-23 21:11:22 +0800

    add:add_admin

---
+++ event/events/group/add_admin.py
@@ -0,0 +1,38 @@
+import time
+
+from containers import Group, ReturnData, EventContainer, User
+from event.base_event import BaseEvent
+
+
+class AddAdmin(BaseEvent):
+    auth = True
+
+    def _run(self, group_id, member_id):
+        with self.server.db_group.enter(group_id) as g:
+            group: Group = g.value
+            if group is None:
+                return ReturnData(ReturnData.NULL, 'Group does not exist.')
+
+            if member_id not in group.member_dict:
+                return ReturnData(ReturnData.NULL, f'No members with id:"{member_id}"')
+
+            if self.user_id not in list(group.admin_list) + [group_id.owner]:
+                return ReturnData(ReturnData.ERROR, 'You are not the admin.')
+
+            if member_id in group.admin_list or member_id == group.owner:
+                return ReturnData(ReturnData.ERROR, 'the member is already an admin')
+
+            group.admin_list.add(member_id)
+        ec = EventContainer(self.server.db_event)
+        ec. \
+            add('type', 'admin_add'). \
+            add('rid', ec.rid). \
+            add('group_id', group_id). \
+            add('time', time.time()). \
+            add('name', member_id)
+        ec.write_in()
+
+        for m in group.member_dict:
+            with self.server.open_user(m) as u:
+                user: User = u.value
+                user.add_user_event(ec)
