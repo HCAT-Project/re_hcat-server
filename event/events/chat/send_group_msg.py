ecdc6a0b3328a1ad9e5ca19c4cc909831f07387d
hsn2 <912372447@qq.com>
2023-01-24 17:53:02 +0800

    fix:send_group_msg:ban

--- event/events/chat/send_group_msg.py
+++ event/events/chat/send_group_msg.py
@@ -21,8 +21,13 @@ class SendGroupMsg(BaseEvent):
             add('user_id', self.user_id). \
             add('msg', escape(msg)). \
             add('time', time.time())
-        ec.write_in()
 
         with self.server.db_group(group_id) as g:
             group: Group = g.value
+            if self.user_id in group.ban_dict:
+                if group.ban_dict[self.user_id]['time'] < time.time():
+                    del group.ban_dict[self.user_id]
+                else:
+                    return ReturnData(ReturnData.ERROR, 'You have been banned by admin.')
             group.broadcast(self.server, self.user_id, escape(msg))
+            ec.write_in()
