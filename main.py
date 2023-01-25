5dbad8353ae1d5ab7e7500df9c7407941f8b6198
hsn2 <912372447@qq.com>
2023-01-25 18:06:57 +0800

    add:logger:FileHandler

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ debug = True
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(asctime)s][%(filename)s(%(lineno)d)][%(levelname)s] %(message)s',
                     datefmt='%b/%d/%Y-%H:%M:%S')
-
+handler = logging.FileHandler('log.txt', encoding='utf8')
+logging.getLogger().addHandler(handler)
 s = Server(debug=debug)
 s.start()
