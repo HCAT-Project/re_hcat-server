96896ea76933ddfd26cc4aa9bdcf12a5f502ca58
hsn2 <912372447@qq.com>
2023-01-25 16:50:09 +0800

    change:rpdb

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import logging
 import os.path
 import subprocess
 
-if not os.path.exists(os.path.join(os.getcwd(), 'RPDB')):
-    subprocess.check_call(['git', 'clone', 'https://github.com/hsn8086/RPDB.git'])
+'''if not os.path.exists(os.path.join(os.getcwd(), 'RPDB')):
+    subprocess.check_call(['git', 'clone', 'https://github.com/hsn8086/RPDB.git'])'''
 from server import Server
 
 debug = True
