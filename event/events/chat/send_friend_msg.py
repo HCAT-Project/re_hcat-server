cbbd1be435909475bd23db454c84b14eb1386a9f
hsn2 <912372447@qq.com>
2023-01-26 23:34:55 +0800

    fix:send_friend_msg:error

--- event/events/chat/send_friend_msg.py
+++ event/events/chat/send_friend_msg.py
@@ -1,3 +1,5 @@
+import copy
+import json
 import time
 
 from containers import User, ReturnData, EventContainer
@@ -10,24 +12,30 @@ class SendFriendMsg(BaseEvent):
 
     def _run(self, friend_id, msg):
         # {"msg_chain":[{"type":type,"msg":msg},{"type":type,"msg":msg}]}
-
+        msg_ = copy.copy(msg)
         with self.server.open_user(self.user_id) as u:
             user: User = u.value
             if friend_id not in user.friend_dict:
                 return ReturnData(ReturnData.NULL, 'The person is not your friend.')
 
-        if 'msg_chain' not in msg or len(msg['msg_chain']):
+        try:
+            if type(msg_) == str:
+                msg_ = json.loads(msg_)
+            if len(msg_['msg_chain']) == 0:
+                raise
+            for i in range(len(msg_['msg_chain'])):
+                if msg_['msg_chain'][i]['type'] == 'text':
+                    msg_['msg_chain'][i]['msg'] = escape(msg_['msg_chain'][i]['msg'])
+
+        except:
             return ReturnData(ReturnData.ERROR, 'Illegal messages.')
-        for i in range(len(msg['msg_chain'])):
-            if msg['msg_chain'][i]['type'] == 'text':
-                msg['msg_chain'][i]['msg'] = escape(msg['msg_chain'][i]['msg'])
 
         ec = EventContainer(self.server.db_event)
         ec. \
             add('type', 'friend_msg'). \
             add('rid', ec.rid). \
             add('user_id', self.user_id). \
-            add('msg', msg). \
+            add('msg', msg_). \
             add('time', time.time())
         ec.write_in()
         with self.server.open_user(friend_id) as u:
