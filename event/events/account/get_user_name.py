dbc44df859c2bf3791e85012e42c21429895f620
hsn2 <912372447@qq.com>
2023-01-27 11:44:09 +0800

    update:marge get_friend_nick and get_user_name

--- event/events/account/get_user_name.py
+++ event/events/account/get_user_name.py
@@ -6,9 +6,19 @@ class GetUserName(BaseEvent):
     auth = False
 
     def _run(self, user_id):
+        with self.server.open_user(self.user_id) as u:
+            user: User = u.value
+            nick = None
+            if user_id in user.friend_dict:
+                nick = user.friend_dict[user_id]['nick']
+
         if self.server.is_user_exist(user_id):
             with self.server.open_user(user_id) as u:
                 user: User = u.value
-                return ReturnData(ReturnData.OK).add('data', user.user_name).jsonify()
+                rt = ReturnData(ReturnData.OK).add('data', user.user_name)
+                if nick:
+                    rt.add('nick', nick)
+                return rt
         else:
             return ReturnData(ReturnData.NULL, 'User does not exist.').jsonify()
+
