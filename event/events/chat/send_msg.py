8cab1123ef130be9a359fbac566fde3cddf1ed1c
hsn2 <912372447@qq.com>
2023-01-27 12:05:48 +0800

    add:send_msg

---
+++ event/events/chat/send_msg.py
@@ -0,0 +1,13 @@
+from event.base_event import BaseEvent
+from send_group_msg import SendGroupMsg
+from send_friend_msg import SendFriendMsg
+
+
+class SendGroupMsg(BaseEvent):
+    auth = True
+
+    def _run(self, target_id: str, msg):
+        if target_id.startswith('0g'):
+            return self.e_mgr.create_event(SendGroupMsg, self.req, self.path)
+        else:
+            return self.e_mgr.create_event(SendFriendMsg, self.req, self.path)
