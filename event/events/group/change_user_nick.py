b03b057123ee0520d14a95134d0ffaeae3118529
hsn2 <912372447@qq.com>
2023-01-27 16:19:52 +0800

    fix:user_id->member_id

--- event/events/group/change_user_nick.py
+++ event/events/group/change_user_nick.py
@@ -7,12 +7,12 @@ from event.base_event import BaseEvent
 class ChangeUserNick(BaseEvent):
     auth = True
 
-    def _run(self, group_id, nick, user_id=None):
-        _user_id = self.user_id if user_id is None else user_id
+    def _run(self, group_id, nick, member_id=None):
+        _user_id = self.user_id if member_id is None else member_id
 
         with self.server.db_group.enter(group_id) as g:
             group: Group = g.value
-            if user_id is not None and self.user_id not in list(group.admin_list) + [group.owner]:
+            if member_id is not None and self.user_id not in list(group.admin_list) + [group.owner]:
                 return ReturnData(ReturnData.ERROR, 'You don\'t have permission.')
 
             if group is None:
