1bb4eaaf7c0b11e4fc862d32ddbd4e1b9617e6d9
hsn2 <912372447@qq.com>
2023-01-27 16:44:56 +0800

    update:get_groups

--- event/events/group/get_groups.py
+++ event/events/group/get_groups.py
@@ -8,4 +8,12 @@ class GetGroups(BaseEvent):
     def _run(self):
         with self.server.open_user(self.user_id) as u:
             user: User = u.value
-            return ReturnData(ReturnData.OK).add('data', {i:{'remark':user.groups_dict[i]['remark']} for i in list(user.groups_dict)})
+            return ReturnData(ReturnData.OK).add('data',
+                                                 {i: {
+                                                     'remark': user.groups_dict[i]['remark'],
+                                                     'group_name': self.server.db_group.get(i).name,
+                                                     'nick': self.server.db_group.get(i).member_dict[self.user_id][
+                                                         'nick']
+
+                                                 }
+                                                     for i in list(user.groups_dict)})
