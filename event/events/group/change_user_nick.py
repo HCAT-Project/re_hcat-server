577ffce746d338ca4efb803e40533a0a4ae9dcb2
hsn2 <912372447@qq.com>
2023-01-27 16:15:36 +0800

    update:admin can change member nick now

--- event/events/group/change_user_nick.py
+++ event/events/group/change_user_nick.py
@@ -6,14 +6,18 @@ from html import escape
 class ChangeUserNick(BaseEvent):
     auth = True
 
-    # todo:admin change member nick
-    def _run(self, group_id, nick):
+    def _run(self, group_id, nick, user_id=None):
+        _user_id = self.user_id if user_id is None else user_id
+
         with self.server.db_group.enter(group_id) as g:
             group: Group = g.value
+            if user_id is not None and self.user_id not in list(group.admin_list) + [group.owner]:
+                return ReturnData(ReturnData.ERROR, 'You don\'t have permission.')
+
             if group is None:
                 return ReturnData(ReturnData.NULL, 'Group does not exist.')
 
-            if self.user_id not in group.member_dict:
+            if _user_id not in group.member_dict:
                 return ReturnData(ReturnData.ERROR, 'You are not in the group.')
 
-            group.member_dict['nick'] = escape(nick)
+            group.member_dict[_user_id]['nick'] = escape(nick)
