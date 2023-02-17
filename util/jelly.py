a6d400e6344c1cb0300a5a6abbeed4fb5d3d0aaa
hsn <hsn1919810@gmail.com>
2023-02-17 21:20:48 +0800

    update:jelly

--- util/jelly.py
+++ util/jelly.py
@@ -1,25 +1,36 @@
 class Jelly:
+    """
+    A class for pickling and unpickling instances of itself
+    """
+
     # auther: hsn
     # data: 01/15/2023(MM/DD/YYYY)
-    # ver: 1.0
+    # ver: 2.0
     def __init__(self):
         self._var_init()
 
     def _var_init(self):
+        # Initialize instance variables
         ...
 
-    def _get_self_var(self) -> list:
-        rt_l = []
-        [rt_l.append(i)
-         if (not i.startswith('__')) and (type(getattr(self, i)).__name__ != 'method')
-         else None
-         for i in dir(self)]
-        return rt_l
+    def _get_instance_variables(self) -> list:
+        """
+        Get a list of all non-method instance variables
+        """
+        return [i for i in dir(self) if not i.startswith('__') and not callable(getattr(self, i))]
 
     def __getstate__(self):
-        return {k: getattr(self, k) for k in self._get_self_var()}
+        """
+        Get the state of the object for pickling
+        """
+        state = {k: getattr(self, k) for k in self._get_instance_variables()}
+        return state
 
     def __setstate__(self, state):
+        """
+        Set the state of the object after unpickling
+        """
+        state = state
         self._var_init()
-        for k in state:
-            setattr(self, k, state[k])
+        for k, v in state.items():
+            setattr(self, k, v)
