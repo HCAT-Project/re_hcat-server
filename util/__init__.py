ae465674dfdc5fc03659e32a7121d467829333cd
hsn <hsn1919810@gmail.com>
2023-02-17 21:19:20 +0800

    Optimize:util.py

--- util/__init__.py
+++ util/__init__.py
@@ -4,13 +4,21 @@ import hashlib
 import json
 import random
 from html import escape
-
 import pyaes
+from typing import Iterable, Any
+from werkzeug.datastructures import ImmutableMultiDict
+from html import escape
+import copy
+import json
 
 
 class AesCrypto:
     def __init__(self, key: str, mode=pyaes.aes.AESModeOfOperationECB):
-        # get mode
+        """
+        Initializes an AES encryption object with the given key and mode.
+        :param key: The encryption key, should be between 16 and 32 bytes long and an integer multiple of 4 bytes.
+        :param mode: The encryption mode, defaults to ECB.
+        """
         self.mode = mode
         if 16 <= len(key) <= 32 and len(key) % 4 == 0:
             self.key = key
@@ -19,15 +27,25 @@ class AesCrypto:
                 'The AES key length should be greater than 16 bytes and less than 32 bytes and be an integer multiple of 4 bytes.')
         self.aes = self.mode(self.key.encode('utf8'))
 
-    def encrypto(self, data: str):
-        data_bytes = data.encode('utf8')  # 编码
-        data_bytes += bytes([0] * (16 - (len(data_bytes) % 16)))  # 补位
+    def encrypt(self, data: str):
+        """
+        Encrypts the given data using the AES object's key and mode.
+        :param data: The data to be encrypted.
+        :return: The base64-encoded encrypted result.
+        """
+        data_bytes = data.encode('utf8')
+        data_bytes += bytes([0] * (16 - (len(data_bytes) % 16)))
         rt_bytes = bytes([])
         for i in range(int(len(data_bytes) / 16)):
             rt_bytes += self.aes.encrypt(data_bytes[16 * i:16 * (i + 1)])
         return base64.b64encode(rt_bytes).decode('utf8')
 
-    def decrypto(self, cipher_text: str):
+    def decrypt(self, cipher_text: str):
+        """
+        Decrypts the given base64-encoded cipher text using the AES object's key and mode.
+        :param cipher_text: The base64-encoded cipher text to be decrypted.
+        :return: The decrypted data as a string.
+        """
         data_bytes = base64.b64decode(cipher_text)
         rt_bytes = bytes([])
         for i in range(int(len(data_bytes) / 16)):
@@ -36,6 +54,13 @@ class AesCrypto:
 
 
 def salted_hash(data, salt, additional_string=None):
+    """
+    Generates a salted hash for the given data and salt.
+    :param data: The data to be hashed.
+    :param salt: The salt to use for the hash.
+    :param additional_string: An additional string to add to the hash if desired.
+    :return: The salted hash as a hexadecimal string.
+    """
     hash_salt = salt
     if additional_string is not None:
         hash_salt += hashlib.sha1(additional_string.encode('utf8')).hexdigest()
@@ -43,48 +68,72 @@ def salted_hash(data, salt, additional_string=None):
 
 
 def get_random_token(key_len=128, upper=True):
+    """
+    Generates a random token of the specified length.
+    :param key_len: The length of the token to be generated.
+    :param upper: If True, include uppercase letters in the token. Otherwise, only use lowercase and digits.
+    :return: The generated token as a string.
+    """
     if upper:
-        return ''.join(
-            [chr(random.choice(list(range(65, 91)) + list(range(97, 123)) + list(range(48, 58)))) for _ in
-             range(key_len)])
+        choices = list(range(65, 91)) + list(range(97, 123)) + list(range(48, 58))
     else:
-        return ''.join(
-            [chr(random.choice(list(range(97, 123)) + list(range(48, 58)))) for _ in range(key_len)])
+        choices = list(range(97, 123)) + list(range(48, 58))
+    return ''.join([chr(random.choice(choices)) for _ in range(key_len)])
 
 
-def ins(obj: iter, collection) -> bool:
-    res = True
-    for i in obj:
-        res = res and (i in collection)
-    return res
+def ins(obj: Iterable, collection: Iterable) -> bool:
+    """
+    Checks if all elements in an iterable are in another iterable.
+
+    :param obj: Iterable to check.
+    :param collection: Iterable to check against.
+    :return: True if all elements in obj are in collection, False otherwise.
+    """
+    return all(elem in collection for elem in obj)
 
 
-def not_ins(obj: iter, collection) -> bool:
-    res = True
-    for i in obj:
-        res = res and (i not in collection)
-    return res
+def not_ins(obj: Iterable, collection: Iterable) -> bool:
+    """
+    Checks if all elements in an iterable are not in another iterable.
+
+    :param obj: Iterable to check.
+    :param collection: Iterable to check against.
+    :return: True if all elements in obj are not in collection, False otherwise.
+    """
+    return all(elem not in collection for elem in obj)
 
 
-def request_parse(req_data):
+def request_parse(req_data: ImmutableMultiDict) -> dict:
+    """
+    Parses the data from a request.
+
+    :param req_data: ImmutableMultiDict representing the data in the request.
+    :return: A dictionary containing the data in the request.
+    """
     if req_data.method == 'POST':
         data = dict(req_data.form)
     elif req_data.method == 'GET':
         data_dict = {}
-        for i in req_data.args.items():
-            data_dict[i[0]] = i[1]
+        for key, value in req_data.args.items():
+            data_dict[key] = value
         data = data_dict
     else:
         data = {}
     return data
 
 
-def msg_process(msg):
+def msg_process(msg: Any) -> dict:
+    """
+    Processes a message, escaping text messages and raising an error if the message chain is empty.
+
+    :param msg: The message to process.
+    :return: The processed message as a dictionary.
+    """
     msg_ = copy.copy(msg)
-    if type(msg_) == str:
+    if isinstance(msg_, str):
         msg_ = json.loads(msg_)
     if len(msg_['msg_chain']) == 0:
-        raise
+        raise ValueError("Message chain is empty.")
     for i in range(len(msg_['msg_chain'])):
         if msg_['msg_chain'][i]['type'] == 'text':
             msg_['msg_chain'][i]['msg'] = escape(msg_['msg_chain'][i]['msg'])
