d57efd08f6a96e679f823eacf4a9b74dc75aeff9
hsn <hsn1919810@gmail.com>
2023-02-17 18:35:33 +0800

    Optimize:server.py

--- server.py
+++ server.py
@@ -21,26 +21,24 @@ class Server:
     ver = '2.0.1'
 
     def __init__(self, address: tuple[str, int] = None, debug: bool = False, name=__name__, config=None):
-        # init Flask object
+        # Initialize Flask object
         self.app = Flask(__name__)
+        # Enable Cross-Origin Resource Sharing (CORS)
         CORS(self.app)
 
-        # get the host and port from the address
+        # Set host and port for the server
         self.host, self.port = address if address is not None else ('0.0.0.0', 8080)
 
-        # set debug mode
+        # Set debug mode
         self.debug = debug
 
-        # init config
-        if config is None:
-            self.config = {}
-        else:
-            self.config = copy.deepcopy(config)
+        # Initialize config
+        self.config = {} if config is None else copy.deepcopy(config)
 
-        # get logger
+        # Get logger
         self.logger = logging.getLogger(__name__)
 
-        # generate aes token
+        # Generate AES token
         key_path = os.path.join(os.getcwd(), f'{name}.key')
         if not os.path.exists(key_path):
             self.key = util.get_random_token(16)
@@ -50,34 +48,37 @@ class Server:
             with open(key_path, 'r', encoding='utf8') as f:
                 self.key = f.read()
 
-        # set event manager
+        # Set event manager
         self.e_mgr = EventManager(self)
-        # set timeout
-        self.event_timeout = 604_800
+
+        # Set timeout
+        self.event_timeout = 604_800  # 1 week
+
+        # Keep track of active users
         self.activity_dict = {}
         self.activity_dict_lock = threading.Lock()
-        # init database
+
+        # Initialize databases
         self.db_account = RPDB(os.path.join(os.getcwd(), 'data', 'account'))
         self.db_event = RPDB(os.path.join(os.getcwd(), 'data', 'event'))
         self.db_group = RPDB(os.path.join(os.getcwd(), 'data', 'group'))
 
     def server_thread(self):
-
-        # start wsgi server
+        # Start the WSGI server
         server = pywsgi.WSGIServer((self.host, self.port), self.app)
         server.serve_forever()
 
     def activity_list_thread(self):
+        # Monitor the activity of users and mark them as offline if they are inactive
         while True:
             del_list = []
-            self.activity_dict_lock.acquire()
-            for i in self.activity_dict:
-                self.activity_dict[i] -= 1
-                if self.activity_dict[i] <= 0:
-                    del_list.append(i)
-            for i in del_list:
-                self.activity_dict.pop(i)
-            self.activity_dict_lock.release()
+            with self.activity_dict_lock:
+                for i in self.activity_dict:
+                    self.activity_dict[i] -= 1
+                    if self.activity_dict[i] <= 0:
+                        del_list.append(i)
+                for i in del_list:
+                    self.activity_dict.pop(i)
 
             for i in del_list:
                 with self.open_user(i) as u:
@@ -86,46 +87,50 @@ class Server:
             time.sleep(1)
 
     def event_cleaner_thread(self):
+        # Remove expired events from the event database
         while True:
             for i in self.db_event.keys:
                 with self.db_event.enter(i) as v:
-                    # THERE IS NO BUG HERE!!!
-                    # I don't understand why PyCharm think it is a bug
-                    if time.time() - v.value['time'] > self.event_timeout:
-                        # del event if event timeout
+                    if v.value and time.time() - v.value['time'] > self.event_timeout:
                         v.value = None
             time.sleep(30)
 
     def start(self):
-
+        # Log server start
         self.logger.info('Starting server...')
+
+        # Create route for handling incoming requests
         self.logger.info('Creating route...')
 
         @self.app.route('/api/<path:path>', methods=['GET', 'POST'])
         def recv(path):
             return self.e_mgr.create_event(RecvEvent, request, path)
 
-        self.logger.info('Starting server thread...')
-        t_server = threading.Thread(target=self.server_thread, daemon=True, name='server_thread')
-        t_cleaner = threading.Thread(target=self.event_cleaner_thread, daemon=True, name='event_cleaner_thread')
-        t_activity = threading.Thread(target=self.activity_list_thread, daemon=True, name='activity_thread')
-        t_server.start()
-        t_cleaner.start()
-        t_activity.start()
-        self.logger.info('Server is already started.')
-        self.logger.info(f'Server is listening to {self.host}:{self.port}.')
-        self.logger.info('----Server is loaded----')
-        self.logger.info('Version: ' + self.ver)
-        self.logger.info('Py ver: ' + sys.version)
-        self.logger.info('SYS ver: ' + platform.platform())
-        self.logger.info('Debug: ' + str(self.debug))
-        self.logger.info('CWD: ' + os.getcwd())
-        self.logger.info('------------------------')
-        while True:
-            try:
-                t_server.join(0.1)
-            except KeyboardInterrupt:
-                sys.exit()
+        # Start server threads
+        self.logger.info('Starting server threads...')
+        server_thread = threading.Thread(target=self.server_thread, daemon=True, name='server_thread')
+        cleaner_thread = threading.Thread(target=self.event_cleaner_thread, daemon=True, name='event_cleaner_thread')
+        activity_thread = threading.Thread(target=self.activity_list_thread, daemon=True, name='activity_thread')
+        threads = [server_thread, cleaner_thread, activity_thread]
+        for t in threads:
+            t.start()
+
+        # Log server status and information
+        self.logger.info('Server started.')
+        self.logger.info(f'Server listening on {self.host}:{self.port}.')
+        self.logger.info('----Server information----')
+        self.logger.info(f'Version: {self.ver}')
+        self.logger.info(f'Python version: {sys.version}')
+        self.logger.info(f'System version: {platform.platform()}')
+        self.logger.info(f'Debug mode: {self.debug}')
+        self.logger.info(f'Current working directory: {os.getcwd()}')
+        self.logger.info('--------------------------')
+
+        try:
+            # Wait for the server thread to finish
+            server_thread.join(0.1)
+        except KeyboardInterrupt:
+            sys.exit()
 
     def open_user(self, user_id):
         return self.db_account.enter(user_id)
