5165eb5c67c37804917217ef2e8cd0589f642cc1
hsn <hsn1919810@gmail.com>
2023-02-18 14:48:40 +0800

    fix:status:user does not exist

--- event/events/account/status.py
+++ event/events/account/status.py
@@ -6,6 +6,8 @@ class Status(BaseEvent):
     auth = False
 
     def _run(self, user_id):
+        if user_id[0] in [str(i) for i in range(10)] and user_id[1] == 's':
+            return ReturnData(ReturnData.OK).add('status', 'online')
         if not self.server.is_user_exist(user_id):
             return ReturnData(ReturnData.NULL, 'User does not exist.')
         with self.server.open_user(user_id) as u:
