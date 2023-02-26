88655345313b44ad895b3004a74be4be11c22641
hsn <hsn1919810@gmail.com>
2023-02-26 16:47:08 +0800

    update:auxiliary event

--- event/event_manager.py
+++ event/event_manager.py
@@ -22,11 +22,20 @@ class EventManager:
 
         # run auxiliary events
         ae_rt = None
+        cancel = False
         if event in self.auxiliary_events:
             for e in self.auxiliary_events[event]:
                 ae_rt_temp = self.create_event(e, req, path)
-                if ae_rt_temp != ReturnData(ReturnData.NULL, '').jsonify():
-                    ae_rt = ae_rt_temp
+                if ae_rt_temp is not None:
+                    if not isinstance(ae_rt_temp,tuple) or len(ae_rt_temp) == 1:
+                        if isinstance(ae_rt_temp, bool):
+                            ae_rt_temp = (ae_rt_temp, ReturnData(ReturnData.NULL, '').jsonify())
+                        else:
+                            ae_rt_temp = (False, ae_rt_temp)
+
+                    if ae_rt_temp[1] != ReturnData(ReturnData.NULL, '').jsonify():
+                        ae_rt = ae_rt_temp[1]
+                    cancel = ae_rt_temp[0] or cancel
 
         if 'auth_data' in req.cookies:
             auth_data = req.cookies['auth_data']
@@ -43,10 +52,14 @@ class EventManager:
         if not event.auth:
             auth_success = True
         if auth_success:
-            e = event(self.server, req, path, self, j['user_id'])
-            rt = e.run()
-            if type(rt) == ReturnData:
-                rt = rt.jsonify()
-            return rt if rt is not None else (ae_rt if ae_rt is not None else ReturnData(ReturnData.NULL, '').jsonify())
+            if not cancel:
+                e = event(self.server, req, path, self, j['user_id'])
+                rt = e.run()
+                if type(rt) == ReturnData:
+                    rt = rt.jsonify()
+                return rt if rt is not None else (
+                    ae_rt if ae_rt is not None else ReturnData(ReturnData.NULL, '').jsonify())
+            else:
+                return ae_rt if ae_rt is not None else ReturnData(ReturnData.NULL, '').jsonify()
         else:
             return ReturnData(ReturnData.ERROR, 'Invalid token.').jsonify()
