92fd427648689c0e1eee58394333a704bb797f83
hsn <hsn1919810@gmail.com>
2023-03-03 18:11:27 +0800

    fix

--- util/command_parser.py
+++ util/command_parser.py
@@ -9,7 +9,18 @@
 
 @Version    : 1.0.1
 """
-
+#  Copyright (C) 2023. HCAT-Project-Team
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import copy
 # Copyright 2023. hsn
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -115,12 +126,14 @@ class Command:
     def __getitem__(self, item):
 
         if isinstance(item, int):
-            
+
             if item < len(self.cmd_list):
                 return self.cmd_list[item]
             else:
                 return ''
-        return self.cmd_list.__getitem__(item)
+        cmd = copy.deepcopy(self)
+        cmd.cmd_list = cmd.cmd_list[item]
+        return cmd
 
     def __setitem__(self, key, value):
         return self.cmd_list.__setitem__(key, value)
