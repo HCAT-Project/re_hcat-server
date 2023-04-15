99aa2bdbe19d7b17a5b0f5b376051979c8d8771f
hsn <hsn1919810@gmail.com>
2023-04-15 13:33:48 +0800

    add:base_receiver

--- src/request_receiver/base_receiver.py
+++ src/request_receiver/base_receiver.py
@@ -22,6 +22,42 @@
 
 @Version    : 1.0.0
 """
+import random
+import threading
+
+from src.containers import ReturnData
+from src.util.config_parser import ConfigParser
+
+
 class BaseReceiver:
-    def __init__(self):
+    def __init__(self, callback=None, config: ConfigParser = None):
+        self.callback = callback
+        self.lock = threading.Lock()
+        self.config = config if config is not None else ConfigParser({})
+        self.host = self.config.get_from_pointer(f"/receivers/{type(self).__name__}/host", "0.0.0.0")
+        self.port = self.config.get_from_pointer(f"/receivers/{type(self).__name__}/port", random.randint(10000, 65535))
+
+    def pause(self):
+        self.lock.acquire()
+
+    def resume(self):
+        self.lock.release()
+
+    def set_callback(self, callback=None):
+        self.callback = callback
+
+    def start(self):
+        threading.Thread(target=self._start, daemon=True).start()
+
+    def _start(self):
         ...
+
+    def create_req(self, req=None):
+        self.lock.acquire()
+        rt = ReturnData(ReturnData.ERROR)
+        try:
+            rt = self.callback(req)
+        finally:
+            self.lock.release()
+            # format return data
+            return rt
