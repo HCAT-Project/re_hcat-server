754aba5738179bce7ada31a3342c3b03e878d83d
hsn <hsn1919810@gmail.com>
2023-04-22 21:08:49 +0800

    fix:Users can add themselves as friends.

--- src/event/events/friend/add_friend.py
+++ src/event/events/friend/add_friend.py
@@ -31,9 +31,13 @@ class AddFriend(BaseEvent):
     auth = True
 
     def _run(self, user_id, add_info=''):
-        _=self.gettext_func
+        _ = self.gettext_func
         if not self.server.is_user_exist(user_id):
             return ReturnData(ReturnData.NULL, _('User does not exist.')).jsonify()
+
+        if user_id == self.user_id:
+            return ReturnData(ReturnData.ERROR, _('You cannot add yourself as a friend.')).jsonify()
+
         with self.server.open_user(self.user_id) as u:
             user: User = u.value
             if user_id in user.friend_dict:
