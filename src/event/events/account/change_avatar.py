e0750b071d080940510e77b0a0b3cd0da65236b8
hsn <hsn1919810@gmail.com>
2023-07-04 21:23:30 +0800

    add: change_avatar.py

---
+++ src/event/events/account/change_avatar.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#  Copyright (C) 2023. HCAT-Project-Team
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+@File       : change_avatar.py
+
+@Author     : hsn
+
+@Date       : 7/4/23 8:16 PM
+
+@Version    : 1.0.0
+"""
+from PIL import Image
+
+from src.containers import ReturnData
+from src.event.base_event import BaseEvent
+from src.util.file_manager import FileManager
+
+
+class ChangeAvatar(BaseEvent):
+    auth = True
+
+    def _run(self, file_hash):
+        _ = self.gettext_func
+        with self.server.update_user_data(self.user_id) as user:
+            f_mgr: FileManager = self.server.upload_folder
+            if file_path := f_mgr.get_file_path(file_hash):
+                try:
+                    with Image.open(file_path) as img:
+                        width, height = img.size
+                        if width != height:
+                            return ReturnData(ReturnData.ERROR, _('Image is not square.'))
+                        if width > 1024:
+                            return ReturnData(ReturnData.ERROR, _('Image is too large.'))
+                except IOError:
+                    return ReturnData(ReturnData.ERROR, _('Image is not valid.'))
+                user.user_avatar = file_hash
+                return ReturnData(ReturnData.OK)
+            else:
+                return ReturnData(ReturnData.ERROR, _('File not found.'))
