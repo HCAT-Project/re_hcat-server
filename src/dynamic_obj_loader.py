29385780c3a26e72fb713481e28962197bb6ace5
hsn <hsn1919810@gmail.com>
2023-10-29 21:19:00 +0800

    Add: type check

--- src/dynamic_obj_loader.py
+++ src/dynamic_obj_loader.py
@@ -38,49 +38,54 @@ class DynamicObjLoader:
         self.group_dict = {}
 
     @staticmethod
-    def load_obj(path: str | PosixPath, obj_name: str = ""):
+    def load_obj(path: str | PosixPath, obj_name: str = "", *, _type: type = None):
         path = str(path)
         if obj_name == "":
             obj_name = src.util.text.under_score_to_pascal_case(Path(path).stem)
 
-        try:
-            m_name = f'{path.replace("/", ".").replace(".py", "")}'
+        m_name = f'{path.replace("/", ".").replace(".py", "")}'
 
-            # get the module
-            module_ = importlib.import_module(m_name)
+        # get the module
+        module_ = importlib.import_module(m_name)
 
-            # get the class of the event
-            obj_ = getattr(module_, obj_name)
+        # get the class of the event
+        obj_ = getattr(module_, obj_name)
 
-        except ImportError:
-
-            return None
+        if _type is not None and not isinstance(obj_, _type):
+            print(obj_, _type)
+            raise TypeError(f"obj {obj_name} in {path} is not a {_type}")
 
         return obj_
 
-    def load_objs(self, path: Union[str, Path]):
+    def load_objs(self, path: Union[str, Path], *, _type: type = None):
         modules_dir = path if isinstance(path, Path) else Path(path)
 
         for i in filter(lambda p: p.suffix == '.py', modules_dir.iterdir()):
             if i.is_dir():
-                yield from self.load_objs(i)
+                yield from self.load_objs(path=i, _type=_type)
             else:
-                obj_name = src.util.text.under_score_to_pascal_case(i.stem)
-                rt_obj = self.load_obj(i.relative_to(Path.cwd()).as_posix(), obj_name)
+                obj_name = src.util.text.under_score_to_pascal_case(name=i.stem)
+
+                try:
+                    rt_obj = self.load_obj(path=i.relative_to(Path.cwd()).as_posix(), obj_name=obj_name, _type=_type)
+                except ImportError:
+                    continue
+
                 if rt_obj is not None:
                     yield rt_obj
 
-    def load_objs_from_group(self, group: str = "default"):
+    def load_objs_from_group(self, group: str = "default", *, _type: type = None):
         for i in self.group_dict.get(group, []):
-            yield from self.load_objs(i)
+            yield from self.load_objs(i, _type=_type)
 
-    def load_obj_from_group(self, path: Union[str, Path], obj_name: str = "", group: str = "default"):
+    def load_obj_from_group(self, path: Union[str, Path], obj_name: str = "", group: str = "default", *,
+                            _type: type = None):
         for i in self.group_dict.get(group, []):
             module_path = Path(i) / path
             if module_path.with_suffix(".py").exists():
                 module_path = module_path.resolve()
 
-                return self.load_obj(module_path.relative_to(Path.cwd()).as_posix(), obj_name=obj_name)
+                return self.load_obj(module_path.relative_to(Path.cwd()).as_posix(), obj_name=obj_name, _type=_type)
         return None
 
     def add_path_to_group(self, group: str = "default", path: Union[str, Path] = ""):
